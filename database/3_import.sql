e90ec69ed6c3d4d74e4ac38f550196c4ed740f7a
ANASTASIJA <anastassija.ivanova@gmail.com>
2025-06-02 13:37:06 +0300

    goomerid ja city

--- database/3_import.sql
+++ database/3_import.sql
@@ -24,16 +24,16 @@ INSERT INTO groomly.groomer (id, user_id, location_id, name, description, tel_nu
 
 INSERT INTO groomly.booking (id, groomer_id, customer_user_id, status, total_price, date) VALUES (default, 1, 3, 'A', 140.00, '2025-05-23');
 
-INSERT INTO groomly.procedure (id, name, description, status) VALUES (3, 'lõikus', e'masina ja kääridega lõikus
-', 'A');
-INSERT INTO groomly.procedure (id, name, description, status) VALUES (2, 'pesemine', e'pesemine, pulstunud karva kammimine
-', 'A');
-INSERT INTO groomly.procedure (id, name, description, status) VALUES (4, 'hügieeni hooldus', e'küünte lõikus, kõrvade ja silmade puhastamine
-', 'A');
+INSERT INTO groomly.procedure (id, name, description, status) VALUES (3, 'lõikus', e'masina ja kääridega lõikus', 'A');
+INSERT INTO groomly.procedure (id, name, description, status) VALUES (2, 'pesemine', e'pesemine, pulstunud karva kammimine', 'A');
+INSERT INTO groomly.procedure (id, name, description, status) VALUES (4, 'hügieeni hooldus', e'küünte lõikus, kõrvade ja silmade puhastamine', 'A');
 INSERT INTO groomly.procedure (id, name, description, status) VALUES (1, 'täispakett', 'hügieeni hooldus, pesemine, lõikus', 'A');
 
-INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (default, 1, 1, 90.00);
-INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (default, 1, 2,50.00 );
+INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (1, 1, 1, 90.00);
+INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (2, 1, 2, 50.00);
+INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (3, 1, 3, 20.00);
+INSERT INTO groomly.groomer_procedure (id, groomer_id, procedure_id, price) VALUES (4, 3, 1, 100.00);
+
 
 INSERT INTO groomly.booking_procedure (id, booking_id, procedure_id, price) VALUES (default, 1, 1, 90.00);
 INSERT INTO groomly.booking_procedure (id, booking_id, procedure_id, price) VALUES (default, 1, 2, 40.00);
